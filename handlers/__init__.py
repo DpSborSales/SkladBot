1b9a5851efa3d933337c54233a367d375490cadd
DpSborSales <dippelwb@gmail.com>
2026-03-02 01:12:21 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,9 +2,11 @@ from .common import register_common_handlers
 from .edit_order import register_edit_handlers
 from .transfer import register_transfer_handlers
 from .payments import register_payment_handlers
+from .admin import register_admin_handlers
 
 def register_all_handlers(bot):
     register_common_handlers(bot)
     register_edit_handlers(bot)
     register_transfer_handlers(bot)
     register_payment_handlers(bot)
+    register_admin_handlers(bot)
